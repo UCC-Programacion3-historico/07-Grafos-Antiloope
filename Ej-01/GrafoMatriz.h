45fa2c181f2175c6eb43887e700e116a2326ba04
Antiloope <rodrigopizarro1234@gmail.com>
2017-10-25 19:06:05 -0300

    Terminado ejercicio 1

--- Ej-01/GrafoMatriz.h
+++ Ej-01/GrafoMatriz.h
@@ -5,8 +5,10 @@
 template<class T>
 class GrafoMatriz {
 private:
-
-
+    T** Nodos;
+    int** PesoArcos;
+    bool** Arcos;
+    int tam;
 public:
     GrafoMatriz(int size);
 
@@ -18,7 +20,7 @@ public:
 
     void nodo_set_val(int n, T val);
 
-    int nodo_get_val(int n2);
+    T nodo_get_val(int n2);
 
 
     // Metodos de Arcos
@@ -41,62 +43,112 @@ public:
 
 template<class T>
 GrafoMatriz<T>::GrafoMatriz(int size) {
-
+    tam = size;
+    Nodos = new T*[size];
+    Arcos = new bool*[size];
+    PesoArcos = new int*[size];
+    for (int i = 0; i < size; i++) {
+        Nodos[i] = NULL;
+        Arcos[i] = new bool[size];
+        PesoArcos[i] = new int[size];
+        for (int j = 0; j < size; j++) {
+            Arcos[i][j] = 0;
+        }
+    }
 }
 
 template<class T>
 void GrafoMatriz<T>::nodo_agregar(int n, T val) {
-
+    if (Nodos[n] == NULL && n < tam) {
+        Nodos[n] = new T;
+        *Nodos[n] = val;
+        return;
+    }
+    throw 1;
 }
 
 template<class T>
 void GrafoMatriz<T>::nodo_remover(int n) {
-
+    if (Nodos[n] == NULL) {
+        throw 1;
+    }
+    if (n < tam) {
+        delete Nodos[n];
+        Nodos[n] = NULL;
+    }
 }
 
 template<class T>
 void GrafoMatriz<T>::nodo_set_val(int n, T val) {
-
+    if (n < tam && Nodos[n] != NULL) {
+        *Nodos[n] = val;
+        return;
+    }
+    throw 1;
 }
 
 template<class T>
-int GrafoMatriz<T>::nodo_get_val(int n2) {
-    return 0;
+T GrafoMatriz<T>::nodo_get_val(int n2) {
+    if (n2 < tam && Nodos[n2] != NULL) {
+        return *Nodos[n2];
+    }
+    throw 1;
 }
 
 template<class T>
 void GrafoMatriz<T>::arco_agregar(int n1, int n2, int val) {
-
+    if (Arcos[n1][n2] == 1 || n1 > tam || n2 > tam || Nodos[n1] == NULL || Nodos[n2] == NULL) {
+        throw 1;
+    }
+    Arcos[n1][n2] = 1;
+    PesoArcos[n1][n2] = val;
 }
 
 template<class T>
 void GrafoMatriz<T>::arco_remover(int n1, int n2) {
-
+    if (n1 > tam || n2 > tam) {
+        throw 1;
+    }
+    Arcos[n1][n2] = 0;
 }
 
 template<class T>
 void GrafoMatriz<T>::arco_set_val(int n1, int n2, int val) {
-
+    if (Arcos[n1][n2] == 0) {
+        throw 1;
+    }
+    PesoArcos[n1][n2] = val;
 }
 
 template<class T>
 int GrafoMatriz<T>::arco_get_val(int n1, int n2) {
-    return 0;
+    if (n1 > tam || n2 > tam || Arcos[n1][n2] == 0) {
+        throw 1;
+    }
+    return PesoArcos[n1][n2];
 }
 
 template<class T>
 int GrafoMatriz<T>::adyacentes(int n1, int n2) {
-    return 0;
+    if (n1 > tam || n2 > tam) {
+        throw 1;
+    }
+    return Arcos[n1][n2];
 }
 
 template<class T>
 int *GrafoMatriz<T>::vecinos(int n) {
-    return nullptr;
+    return Arcos[n];
 }
 
 template<class T>
 bool GrafoMatriz<T>::esVacio() {
-    return false;
+    for (int i = 0; i < tam; i++) {
+        if (Nodos[i] != NULL) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <string>
 #include "GrafoMatriz.h"
 
-int main() {
-    GrafoMatriz<int> gr(4);
+using namespace std;
 
-    std::cout << "Ejercicio 07/01\n" << std::endl;
+int main() {
+    GrafoMatriz<string> gr(4);
+
+    gr.nodo_agregar(1,"Hola");
+    gr.nodo_agregar(2,"Como");
+    gr.nodo_agregar(3,"Estas");
+
+    gr.arco_agregar(1,2,20);
+
+
+    for (int k = 0; k < 4; k++) {
+        try {
+            cout << gr.nodo_get_val(k) << " ";
+        }catch (int e) {
+            cout << "NULL ";
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            try {
+                cout << gr.arco_get_val(i,j) << " ";
+            } catch (int e) {
+                cout << "0 ";
+            }
+
+        }
+        cout << '\n';
+    }
     return 0;
 }
